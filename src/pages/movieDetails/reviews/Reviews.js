170bad755862c8463449fe9d243e42e86fd6d789
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-04-22 19:21:38 +0200

    update

--- src/pages/movieDetails/reviews/Reviews.js
+++ src/pages/movieDetails/reviews/Reviews.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import fetchMovieReviews from "../../../hooks/fetchMovieReviews";
-import css from "./Reviews.module.css";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import fetchMovieReviews from '../../../hooks/fetchMovieReviews';
+import css from './Reviews.module.css';
+import parse from 'html-react-parser';
 
 export default function Reviews() {
   const { movieId } = useParams();
@@ -18,17 +19,20 @@ export default function Reviews() {
   if (!reviews || reviews.length === 0) {
     return <div>No reviews found for this movie.</div>;
   }
-
+  console.log(reviews[1].content);
   return (
     <>
       <h1 className={css.header}>Reviews</h1>
       <ul className={css.list}>
-        {reviews.map((review) => (
+        {reviews.map(review => {
+          const parsed = parse(review.content)
+          return (
           <li key={review.id} className={css.listItem}>
             <h3 className={css.author}>{review.author}</h3>
-            <p className={css.review}>{review.content}</p>
-          </li>
-        ))}
+
+            <p className={css.review}>{parsed}</p>
+          </li>);
+        })}
       </ul>
     </>
   );
